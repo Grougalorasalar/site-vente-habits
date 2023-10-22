3bf58ea049868a9918b1e1b2de2bdc3dd1f69301
Grougalorasalar <jeremy.theret11@gmail.com>
2023-10-22 18:55:20 +0200

    Add: carousel CardImages

--- frontend/src/GridCards.jsx
+++ frontend/src/GridCards.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react'
 import Card from './Card.jsx'
-// import { v4 } from 'uuid'
-
-// import React from 'react';
-// import Card from './Card';
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+}
 
-export function GridCards() {
+function GridCards() {
     let [children, setChildren] = useState([]);
 
     function addCard() {
-        children = setChildren(children.concat(<Card nameArticle={"clothes " + children.length} key={children.length} soldPrice={children.length % 2} price={getRandomInt(179)+"€"}/>))
+        children = setChildren(children.concat(<Card
+            images={["https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/46404db8-e562-4e96-b8ef-6aea0aea335a/sweat-a-capuche-sportswear-club-fleece-QkdkZ2.png",
+                "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/04d31a3e-ec3e-4675-a4d9-7635f86e75b7/sweat-a-capuche-sportswear-club-fleece-QkdkZ2.png",
+                "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/5cd27ad4-82ec-40e1-adc6-3e7e51f1960b/sweat-a-capuche-sportswear-club-fleece-QkdkZ2.png"]}
+            nameArticle={"Clothes " + children.length}
+            key={children.length + 1}
+            soldPrice={children.length % 2}
+            price={getRandomInt(179) + "€"}
+            articleLink={"#article-link"}
+        />))
     }
 
     return (
