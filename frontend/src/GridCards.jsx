976d3b208b967c2e51963070c56d61fc63d0938b
Grougalorasalar <jeremy.theret11@gmail.com>
2023-12-10 11:24:09 +0100

    Fix: responsive GridCard

--- frontend/src/GridCards.jsx
+++ frontend/src/GridCards.jsx
@@ -82,7 +82,7 @@ function GridCards(props) {
                     </select>
                 </div>
             </div>
-            <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+            <section className="w-fit mx-auto grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
                 {
                     articles.map((article) => (
                         <Card
